e10ab4407983d136cbe65464a388fa8bb06607aa
Sean Fridman <mail@seanfridman.com>
2016-04-13 01:56:53 +0200

    f all that jazz

--- lisp/init-fonts.el
+++
@@ -1,5 +0,0 @@
-(require-package 'default-text-scale)
-(global-set-key (kbd "C-M-=") 'default-text-scale-increase)
-(global-set-key (kbd "C-M--") 'default-text-scale-decrease)
-
-(provide 'init-fonts)

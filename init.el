a0973aa215a2270bb7ca9d8cdfa31c254db241c1
Sean Fridman <mail@seanfridman.com>
2016-08-12 15:27:09 +0200

    https://www.youtube.com/watch?v=FEUD_1Trk-0

--- init.el
+++ init.el
@@ -1,17 +1,21 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;;;; el-get
+;; Used to require packages from places other than ELPA MELPA whatever
+
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil t)
+  (with-current-buffer (url-retrieve-synchronously
+			"https://github.com/dimitri/el-get/raw/master/el-get-install.el")
+    (end-of-buffer)
+    (eval-print-last-sexp)))
+
+;; Use this to install external packages
+(setq el-get-packages
+ '(el-get
+   typo))
+
+;;;; MELPA ELPA etc
+;; Set up normal package stuff
 
 (require 'package)
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
@@ -21,33 +25,91 @@
 
 (add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
 
-(package-initialize)
-
 (defun require-package (p)
   (unless (package-installed-p p)
     (package-refresh-contents)
     (package-install p)))
 
-;; General
+;; Bootstrap packages
 
+(package-initialize)
+(el-get 'sync el-get-packages)
+
+;;;; General
+
+;; Customization settings in another file
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+;; Shared emacs instance for otha mutha truckas
+(server-start)
+;; Highlight matching parens
 (show-paren-mode 1)
+;; For line wrap
 (require-package 'visual-fill-column)
+;; Tab control
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 2)
+(setq-default tab-stop-list (number-sequence tab-width 100 tab-width))
+(defvaralias 'standard-indent 'tab-width)
+(defvaralias 'c-basic-offset 'tab-width)
 
-;; Org Mode
+;;;; Dired+
 
+(require-package 'dired+)
+;; Reuse single dired buffer
+(diredp-toggle-find-file-reuse-dir 1)
+
+;;;; Magit
+
+(require-package 'magit)
+(global-set-key (kbd "C-x g") 'magit-status)
+
+;;;; Org Mode
+
+(setq org-fontify-whole-heading-line t)
 (add-hook 'org-mode-hook 'visual-line-mode)
 (add-hook 'org-mode-hook (lambda () (setq fill-column 80)))
 (add-hook 'visual-line-mode-hook 'visual-fill-column-mode)
 
-;; Spellcheck
+;;;; Markdown
+
+(require-package 'markdown-mode)
+(add-hook 'markdown-mode-hook 'visual-line-mode)
+(add-hook 'markdown-mode-hook (lambda () (setq fill-column 80)))
+
+;;;; Spellcheck
 
 (add-hook 'text-mode-hook 'flyspell-mode)
 (add-hook 'prog-mode-hook 'flyspell-prog-mode)
 
-;; Paredit
+;;;; Text-ish modes
+
+(setq-default typo-language 'English)
+(add-hook 'text-mode-hook 'typo-mode)
+
+;;;; Helm
+
+(require-package 'helm)
+(helm-mode 1)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+;; F that no tab completion nonsense
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
+(define-key helm-map (kbd "C-z") 'helm-select-action)
+
+;;;; Projectile
+
+(require-package 'projectile)
+(require-package 'helm-projectile)
+(helm-projectile-on)
+
+;;;; Paredit
 
 (require-package 'paredit)
 (autoload 'enable-paredit-mode "paredit")
+(add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
 (add-hook 'lisp-mode-hook #'enable-paredit-mode)
 (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
 (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
@@ -55,50 +117,76 @@
 (add-hook 'ielm-mode-hook #'enable-paredit-mode)
 (add-hook 'clojure-mode-hook #'enable-paredit-mode)
 (add-hook 'rust-mode-hook #'enable-paredit-mode)
-(add-hook 'cider-repl-mode-hook 'paredit-mode)
+(add-hook 'cider-repl-mode-hook #'enable-paredit-mode)
+(add-hook 'javascript-mode-hook #'enable-paredit-mode)
 
-;; Flycheck
+;;;; Solarized
 
-(require-package 'flycheck)
-(add-hook 'c++-mode-hook 'flycheck-mode)
-(add-hook 'c-mode-hook 'flycheck-mode)
-(add-hook 'rust-mode 'flycheck-mode)
+(if (graphic-display-p) ;; if GUI
+    (progn
+      (require-package 'solarized-theme)
+      (load-theme 'solarized-dark)))
 
-;; Solarized
-
-(require-package 'color-theme-sanityinc-solarized)
-(load-theme 'sanityinc-solarized-light)
-
-;; Clojure
-
-(require-package 'clojure-mode)
-(require-package 'cider)
-
-;; Smex
-
-(require-package 'smex)
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command) ;; old M-x
-
-;; Company Mode completion
+;;;; Company Mode completion
+;; Code completion
 
 (require-package 'company)
 (add-hook 'after-init-hook 'global-company-mode)
 
-;; Flycheck
+;;;; Flycheck
 
 (require-package 'flycheck)
+(require-package 'flycheck-tip)
+(with-eval-after-load 'flycheck (flycheck-pos-tip-mode))
+(add-hook 'clojure-mode-hook 'flycheck-mode)
 (add-hook 'c++-mode-hook 'flycheck-mode)
 (add-hook 'c-mode-hook 'flycheck-mode)
 (add-hook 'rust-mode-hook 'flycheck-mode)
-(add-hook 'rust-mode-hook #'flycheck-rust-setup)
+(add-hook 'yaml-mode 'flycheck-mode)
+(add-hook 'javascript-mode 'flycheck-mode)
 
-;; C / C++
+;;;; Clojure
+
+(require-package 'clojure-mode)
+(require-package 'cider)
+
+;;;; C / C++
 
 (require-package 'cc-mode)
 
-;; Rust
+;;;; Rust
 
 (require-package 'rust-mode)
 (require-package 'flycheck-rust)
+(add-hook 'rust-mode-hook #'flycheck-rust-setup)
+
+;;;; Haskell
+
+(require-package 'haskell-mode)
+
+;;;; Web stuff
+;; web-mode for all kinda templating languages
+(require-package 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
+;; Make syntax highlighting less shitty
+(add-hook 'web-mode-hook
+          (lambda ()
+            (set-face-attribute 'web-mode-html-tag-bracket-face nil :foreground "White")))
+
+;;;; Node / JS
+
+(defvaralias 'js-indent-level 'tab-width)
+(require-package 'nvm)
+(load "nvm") ;; Why do I have to do this?
+(require-package 'js-comint)
+(defun set-node-version (version)
+  (interactive "sNode version: ")
+  (nvm-use version))
+
+;;;; YAML
+
+(require-package 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
